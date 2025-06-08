8a2bdd714b48c17295badddc485e4e29ef0ffa22
xander-che <achernys123@gmail.com>
2025-06-08 09:23:17 +0300

    3 intervals

--- trio_bot.py
+++ trio_bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 import time
 import pandas as pd
 import telegram
@@ -7,21 +8,20 @@ import asyncio
 import logging
 from tinkoff.invest import Client, CandleInterval, HistoricCandle
 from tinkoff.invest.utils import now
-from datetime import timedelta, datetime, timezone
+from datetime import timedelta, datetime
 from ta.trend import MACD, EMAIndicator
 from ta.momentum import RSIIndicator
-from adata import final_df_columns
+from adata import final_df_columns, minutes_hour, minutes_05, minutes_15
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 
 INVEST_TOKEN = os.environ["T_SAND_BOX"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 LOG_BOT_TOKEN = os.environ["LOG_BOT_TOKEN"]
 B_ID = os.environ["B_ID"]
 CHAT_ID = os.environ["CHAT_ID"]
-INTERVALS = [(CandleInterval.CANDLE_INTERVAL_HOUR, '1', 168), (CandleInterval.CANDLE_INTERVAL_4_HOUR, '4', 720)]
+INTERVALS = [(CandleInterval.CANDLE_INTERVAL_HOUR, '1 час', 168, minutes_hour),
+             (CandleInterval.CANDLE_INTERVAL_5_MIN, '5 минут', 24, minutes_05),
+             (CandleInterval.CANDLE_INTERVAL_15_MIN, '15 минут', 24, minutes_15)]
 
 
 def get_logger():
@@ -35,8 +35,6 @@ def get_logger():
 
 
 def async_loop(async_function):
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(async_function)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(async_function)
@@ -46,7 +44,7 @@ def cast_money(v):
     return v.units + v.nano / 1e9
 
 
-def create_df(candles: [HistoricCandle], name: str):
+def create_df(candles: HistoricCandle, name: str):
     df = pd.DataFrame([{
         'fname': name,
         'time': c.time,
@@ -84,14 +82,6 @@ def check_date(data_today):
     today = datetime.today()
     result = False
     if data_today.date() == today.date():
-        result = True
-    return result
-
-
-def check_4h_candle(data_today):
-    result = False
-    four_h_ago = datetime.now(timezone.utc) - timedelta(hours=4)
-    if data_today.hour == four_h_ago.hour:
         result = True
     return result
 
@@ -164,25 +154,21 @@ def get_signal(final_df: pd.DataFrame):
         data_today = final_df.iloc[i]['time']
         close_datetime3 = final_df.iloc[i]['time'] + timedelta(hours=3)
         close_date = str(close_datetime3.date())
-        close_time = close_datetime3.time()
+        close_time = datetime.now().time()
         if close_time.minute == 0:
             close_datetime = f'{close_date} {close_time.hour}:00'
         else:
             close_datetime = f'{close_date} {close_time.hour}:{close_time.minute}'
         date_check = check_date(data_today)
-        if tf == '4':
-            hour_check = check_4h_candle(data_today)
-        else:
-            hour_check = True
         ema_short = get_ema_short(row, 0.0001)
         ema_long = get_ema_long(row, 0.0001)
         macd_short = get_macd_short(row, 0.05)
         macd_long = get_macd_long(row, 0.05)
         rsi_short = get_rsi_short(row)
         rsi_long = get_rsi_long(row)
-        if ema_short is True and macd_short is True and rsi_short is True and date_check is True and hour_check is True:
+        if ema_short and macd_short and rsi_short and date_check:
             res.append([name, tf, 'SHORT', close_datetime, ticker, asset_type, close])
-        elif ema_long is True and macd_long is True and rsi_long is True and date_check is True and hour_check is True:
+        elif ema_long and macd_long and rsi_long and date_check:
             res.append([name, tf, 'LONG', close_datetime, ticker, asset_type, close])
     return res
 
@@ -190,93 +176,99 @@ def get_signal(final_df: pd.DataFrame):
 def get_futures_candles(futures, client, data: list, logger):
     for interval in INTERVALS:
         counter = 0
-        for item in futures.instruments:
-            if item.asset_type == 'TYPE_COMMODITY':
-                try:
-                    candle = client.market_data.get_candles(figi=item.figi,
-                                                            from_=now() - timedelta(hours=interval[2]),
-                                                            to=now(),
-                                                            interval=interval[0])
-                except Exception as exc:
-                    if counter == 0:
-                        logger.exception(f': {exc}')
-                    candle = None
-                    counter += 1
+        print(__name__, datetime.now().minute, 'минут')
+        if str(datetime.now().minute) in interval[3]:
+            print(__name__, 'фьючерсы, совпадение по интервалу ', interval[1])
+            for item in futures.instruments:
+                if item.asset_type == 'TYPE_COMMODITY':
+                    try:
+                        candle = client.market_data.get_candles(figi=item.figi,
+                                                                from_=now() - timedelta(hours=interval[2]),
+                                                                to=now(),
+                                                                interval=interval[0])
+                    except Exception as exc:
+                        if counter == 0:
+                            logger.exception(f': {exc}')
+                        candle = None
+                        counter += 1
 
-                if candle is not None:
-                    if candle.candles.__sizeof__() > 0:
-                        df = create_df(candle.candles, item.name)
-                        if len(df) >= 40:
-                            if not df.is_complete.iloc[-1]:
-                                df = df.drop(axis=0, index=len(df) - 1)
-                            if df.is_complete.iloc[-1]:
-                                df = df.drop('is_complete', axis=1)
-                                df['interval'] = interval[1]
-                                df['ticker'] = item.ticker
-                                df['type'] = 'futures'
-                                df['ema'] = EMAIndicator(close=df['close'], window=10).ema_indicator()
-                                df['macd'] = MACD(close=df['close']).macd()
-                                df['macd_signal'] = MACD(close=df['close']).macd_signal()
-                                df['rsi'] = RSIIndicator(close=df['close']).rsi()
-                                check_counter = 0
-                                tail = df.iloc[-1, :].values.tolist()
-                                for value in tail:
-                                    if pd.isna(value) is True:
-                                        check_counter += 1
-                                if check_counter == 0:
-                                    previous_macd_signal = df.iloc[-2, -2]
-                                    previous_rsi = df.iloc[-2, -1]
-                                    tail.insert(-1, previous_macd_signal)
-                                    tail.append(previous_rsi)
-                                    data.append(tail)
+                    if candle is not None:
+                        if candle.candles.__sizeof__() > 0:
+                            df = create_df(candle.candles, item.name)
+                            if len(df) >= 40:
+                                if not df.is_complete.iloc[-1]:
+                                    df = df.drop(axis=0, index=len(df) - 1)
+                                if df.is_complete.iloc[-1]:
+                                    df = df.drop('is_complete', axis=1)
+                                    df['interval'] = interval[1]
+                                    df['ticker'] = item.ticker
+                                    df['type'] = 'фьючерсы'
+                                    df['ema'] = EMAIndicator(close=df['close'], window=10).ema_indicator()
+                                    df['macd'] = MACD(close=df['close']).macd()
+                                    df['macd_signal'] = MACD(close=df['close']).macd_signal()
+                                    df['rsi'] = RSIIndicator(close=df['close']).rsi()
+                                    check_counter = 0
+                                    tail = df.iloc[-1, :].values.tolist()
+                                    for value in tail:
+                                        if pd.isna(value) is True:
+                                            check_counter += 1
+                                    if check_counter == 0:
+                                        previous_macd_signal = df.iloc[-2, -2]
+                                        previous_rsi = df.iloc[-2, -1]
+                                        tail.insert(-1, previous_macd_signal)
+                                        tail.append(previous_rsi)
+                                        data.append(tail)
     return data
 
 
 def get_shares_candles(shares, client, data: list, logger):
     for interval in INTERVALS:
         counter = 0
-        for item in shares.instruments:
-            if item.currency == 'rub':
-                try:
-                    candle = client.market_data.get_candles(figi=item.figi,
-                                                            from_=now() - timedelta(hours=interval[2]),
-                                                            to=now(),
-                                                            interval=interval[0])
-                except Exception as exc:
-                    if counter == 0:
-                        logger.exception(f': {exc}')
-                    candle = None
-                    counter += 1
-                if candle is not None:
-                    if candle.candles.__sizeof__() > 0:
-                        df = create_df(candle.candles, item.name)
-                        if len(df) >= 40:
-                            if not df.is_complete.iloc[-1]:
-                                df = df.drop(axis=0, index=len(df) - 1)
-                            if df.is_complete.iloc[-1]:
-                                df = df.drop('is_complete', axis=1)
-                                df['interval'] = interval[1]
-                                df['ticker'] = item.ticker
-                                df['type'] = 'shares'
-                                df['ema'] = EMAIndicator(close=df['close'], window=10).ema_indicator()
-                                df['macd'] = MACD(close=df['close']).macd()
-                                df['macd_signal'] = MACD(close=df['close']).macd_signal()
-                                df['rsi'] = RSIIndicator(close=df['close']).rsi()
-                                check_counter = 0
-                                tail = df.iloc[-1, :].values.tolist()
-                                for value in tail:
-                                    if pd.isna(value) is True:
-                                        check_counter += 1
-                                if check_counter == 0:
-                                    previous_macd_signal = df.iloc[-2, -2]
-                                    previous_rsi = df.iloc[-2, -1]
-                                    tail.insert(-1, previous_macd_signal)
-                                    tail.append(previous_rsi)
-                                    data.append(tail)
+        print(__name__, datetime.now().minute, 'минут')
+        if str(datetime.now().minute) in interval[3]:
+            print(__name__, 'акции, совпадение по интервалу ', interval[1])
+            for item in shares.instruments:
+                if item.currency == 'rub':
+                    try:
+                        candle = client.market_data.get_candles(figi=item.figi,
+                                                                from_=now() - timedelta(hours=interval[2]),
+                                                                to=now(),
+                                                                interval=interval[0])
+                    except Exception as exc:
+                        if counter == 0:
+                            logger.exception(f': {exc}')
+                        candle = None
+                        counter += 1
+                    if candle is not None:
+                        if candle.candles.__sizeof__() > 0:
+                            df = create_df(candle.candles, item.name)
+                            if len(df) >= 40:
+                                if not df.is_complete.iloc[-1]:
+                                    df = df.drop(axis=0, index=len(df) - 1)
+                                if df.is_complete.iloc[-1]:
+                                    df = df.drop('is_complete', axis=1)
+                                    df['interval'] = interval[1]
+                                    df['ticker'] = item.ticker
+                                    df['type'] = 'акции'
+                                    df['ema'] = EMAIndicator(close=df['close'], window=10).ema_indicator()
+                                    df['macd'] = MACD(close=df['close']).macd()
+                                    df['macd_signal'] = MACD(close=df['close']).macd_signal()
+                                    df['rsi'] = RSIIndicator(close=df['close']).rsi()
+                                    check_counter = 0
+                                    tail = df.iloc[-1, :].values.tolist()
+                                    for value in tail:
+                                        if pd.isna(value) is True:
+                                            check_counter += 1
+                                    if check_counter == 0:
+                                        previous_macd_signal = df.iloc[-2, -2]
+                                        previous_rsi = df.iloc[-2, -1]
+                                        tail.insert(-1, previous_macd_signal)
+                                        tail.append(previous_rsi)
+                                        data.append(tail)
     return data
 
 
-def get_hour_data(client, logger):
+def get_data(client, logger):
     futures = client.instruments.futures()
     shares = client.instruments.shares()
     data = list()
@@ -292,15 +284,18 @@ async def send_to_bot(response: list, logger):
     bot = telegram.Bot(token=BOT_TOKEN)
     for item in response:
         msg = list()
-        header = '--TRIO--\n'
+        header = f'<b>--TRIO-- {item[1]}</b>\n'
+        empty_str = ' \n'
         name = f'<b>{item[0]}</b>\n'
-        tf = f'Time Frame = {item[1]} h\n'
-        stype = f'Signal type = {item[2]}\n'
+        tf = f'Временной интервал = {item[1]}\n'
+        stype = f'Тип сигнала = {item[2]}\n'
         date_time = f'{item[3]}\n'
-        ticker = f'Ticker = {item[4]}\n'
-        tp = f'Type = {item[5]}\n'
-        close = f'Close = {item[6]:.4f}\n'
+        ticker = f'Тикер = {item[4]}\n'
+        tp = f'Тип инструмента = {item[5]}\n'
+        close = f'Цена закрытия = {item[6]:.4f}\n'
+        donate = '<a href="https://pay.cloudtips.ru/p/84a972ea">Поддержать проект STB:</a>'
         msg.append(header)
+        msg.append(empty_str)
         msg.append(date_time)
         msg.append(name)
         msg.append(tp)
@@ -308,7 +303,9 @@ async def send_to_bot(response: list, logger):
         msg.append(tf)
         msg.append(stype)
         msg.append(close)
-        msg.append('\n')
+        msg.append(empty_str)
+        if random.randint(1, 4) == random.choice([1, 2, 3, 4, 5]):
+            msg.append(donate)
 
         text = ''.join(msg)
         async with bot:
@@ -316,7 +313,7 @@ async def send_to_bot(response: list, logger):
                 await bot.send_message(chat_id=CHAT_ID, text=text, parse_mode=telegram.constants.ParseMode.HTML)
             except telegram.error.BadRequest as exc:
                 logger.exception(f': {exc}')
-            time.sleep(3)
+            time.sleep(1)
 
 
 async def send_logs(logger):
@@ -345,7 +342,7 @@ def run():
     logger = get_logger()
     logger.info(': Run job')
     with Client(INVEST_TOKEN) as client:
-        final_df = get_hour_data(client, logger)
+        final_df = get_data(client, logger)
         response = get_signal(final_df)
     if len(response) > 0:
         loop = asyncio.get_event_loop()
@@ -364,12 +361,12 @@ if __name__ == "__main__":
         main_logger.info(': Program started')
         async_loop(send_logs(main_logger))
         main_logger.handlers = []
-        schedule.every().hour.at(':01').do(run)
+        schedule.every(1).minutes.do(run)
 
         while True:
             try:
                 schedule.run_pending()
-                time.sleep(10)
+                time.sleep(1)
             except Exception as e:
                 main_logger = get_logger()
                 main_logger.exception(f': {e}')
